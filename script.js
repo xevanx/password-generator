7caeef78e8a8fbe707b9dff2d138dc8aa9e4cac2
Evan Wenzel <xevanx2002@gmail.com>
2020-03-01 22:43:00 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+var numb = document;
+var lower;
+var upper;
+var special;
+var ranNumb;
+var ranLower;
+var ranUpper;
+var ranSpecial;
+
+function lowerOnly(){
+};
+
+function upperOnly(){
+};
+
+function specialOnly(){
+};
+
+function numberOnly(){
+};
+
+function lowerUpper(){
+};
+
+function lowerNumb(){
+};
+
+function lowerSpecial(){
+};
+
+function upperNumb(){
+};
+
+function upperSpecial(){
+};
+
+function numbSpecial(){
+};
+
+function lowerUpperNumb(){
+};
+
+function lowerUpperSpecial(){
+};
