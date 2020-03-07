a846526eb42003f568d29ac114b1c33765522a57
Evan Wenzel <xevanx2002@gmail.com>
2020-03-07 13:40:04 -0800

    css fix

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var number = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-var special = ["!", ",", "@", "#", "$", "%", "^", "&", "*", "-", "_", ".", "?", "/", "~"];
 var newPassh1 = document.getElementById("newPass");
 var password = "";
 var genButton = document.getElementById("genButton");
